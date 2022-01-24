fb9aeb4b2dbd63bb5d2b2c957bd3a6370f29f6ad
Andrei Roditelev <rodandr13@gmail.com>
2022-01-24 21:29:57 +0530

    Фильтр и шаблоны авторÐauth

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('users.urls', namespace='users')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
