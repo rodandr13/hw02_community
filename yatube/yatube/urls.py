7f66ac9e6e9c20aa4f5844053fc86adf270ed8be
Andrei Roditelev <rodandr13@gmail.com>
2022-01-24 13:23:02 +0530

    Revert "Добавил новое приложение users"
    This reverts commit 55b0721220c907d34ad03c4fa91ccfd4e7f66367.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
-    path('auth/', include('django.contrib.auth.urls')),
 ]
