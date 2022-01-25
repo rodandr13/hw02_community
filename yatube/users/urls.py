42bc7492151c1658865f6e68e2901ff613f0c435
Andrei Roditelev <rodandr13@gmail.com>
2022-01-25 22:06:16 +0530

    шаÐблоны с паролем

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -3,7 +3,10 @@ from django.contrib.auth.views import (
     LoginView,
     PasswordChangeView,
     PasswordChangeDoneView,
-    PasswordResetView
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
 )
 from django.urls import path
 
@@ -23,11 +26,9 @@ urlpatterns = [
         name='signup'
     ),
     path(
-        'password_change/done/',
-        PasswordChangeDoneView.as_view(
-            template_name='users/password_change_done.html'
-        ),
-        name='password_change_done'
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
     ),
     path(
         'password_change/',
@@ -37,6 +38,13 @@ urlpatterns = [
         name='password_change'
     ),
     path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
+    path(
         'password_reset/',
         PasswordResetView.as_view(
             template_name='users/password_reset_form.html'
@@ -44,8 +52,24 @@ urlpatterns = [
         name='password_reset_form'
     ),
     path(
-        'login/',
-        LoginView.as_view(template_name='users/login.html'),
-        name='login'
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done'
     ),
 ]
