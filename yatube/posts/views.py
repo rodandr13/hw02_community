39007ca3f703f2e5334dcbe5173a18a7b72ac918
Andrei Roditelev <rodandr13@gmail.com>
2022-01-27 12:55:06 +0530

    Добавил модуль about

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
-
 from .models import Post, Group, User
 
 COUNT_ELEMS = 10
@@ -15,14 +14,14 @@ def index(request):
     author = User.objects.get(username='leo')
     start_date = datetime.date(1854, 7, 7)
     end_date = datetime.date(1854, 7, 21)
-    #keyword = 'утро'
+    # keyword = 'утро'
     keyword = ''
     template = 'posts/index.html'
     post_list = (Post.objects
-             .filter(text__contains=keyword)
-             .filter(author=author)
-             .filter(pub_date__range=(start_date, end_date))
-             )
+                 .filter(text__contains=keyword)
+                 .filter(author=author)
+                 .filter(pub_date__range=(start_date, end_date))
+                 )
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -46,3 +45,31 @@ def group_posts(request, slug):
     }
     return render(request, template, context)
 
+
+@login_required
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    post_list = user.posts.all()
+    count_posts = post_list.count()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'count_posts': count_posts,
+        'user': user,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+@login_required
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    post = get_object_or_404(Post, pk=post_id)
+    user = get_object_or_404(User, username=post.author)
+    count_posts = user.posts.count()
+    context = {
+        'post': post,
+        'count_posts': count_posts,
+    }
+    return render(request, 'posts/post_detail.html', context)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts')),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
+    path('about/', include('about.urls', namespace='about')),
+    path('', include('posts.urls', namespace='posts')),
 ]
